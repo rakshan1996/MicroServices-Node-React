1436017bb2138481d764f03ef2e558665b4d99b2
Rakshan Thapar <rakshanthapar32@gmail.com>
2021-04-14 23:56:34 +0530

    QueryService Used to restrict data calls

--- 1stApp/React-App/client/src/CommentList.js
+++ 1stApp/React-App/client/src/CommentList.js
@@ -1,19 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 
-export default({postId})=>{
-    const [comments,setComments]= useState({});
-
-    const fetchComments=async ()=>{
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    };
-
-    useEffect(() => {
-        fetchComments();
-    }, []);
-
+export default({comments})=>{
+   
     const renderComments = Object.values(comments).map(comment => {
         return <li key={comment.id}>
             {comment.content}
