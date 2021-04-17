f67a5f972127a94a8f60afb5617bd3a8423dce24
Rakshan Thapar <rakshanthapar32@gmail.com>
2021-04-17 15:43:57 +0530

    React app updated to moderate status

--- 1stApp/React-App/client/src/CommentList.js
+++ 1stApp/React-App/client/src/CommentList.js
@@ -4,8 +4,20 @@ import React from 'react';
 export default({comments})=>{
    
     const renderComments = Object.values(comments).map(comment => {
+        let content;
+        switch (comment.status){
+            case "Approved":
+                content= comment.content;
+                break;
+            case "Rejected":
+                content = "This comment has been rejected";
+                break;
+            default:
+                content = "This Comment is awaiting moderation";
+        }
+         
         return <li key={comment.id}>
-            {comment.content}
+            {content}
         </li>;
     });
 
